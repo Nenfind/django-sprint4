7a599d07d59d6d79ec1349eb90b58310fe7f72c3
Di <modest.saz@gmail.com>
2025-02-07 16:35:03 +0300

    second iteration | fixed registration view

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
@@ -212,3 +213,13 @@ class CommentDeleteView(OnlyAuthorMixin, ChangeCommentMixin, DeleteView):
     """Delete comment on a post"""
 
     model = Comment
+
+
+class ProfileCreateView(CreateView):
+    template_name='registration/registration_form.html'
+    form_class=UserCreationForm
+
+    def get_success_url(self):
+        """On success redirect to profile page"""
+
+        return reverse('blog:index')
--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -11,15 +11,6 @@ handler500 = 'pages.views.server_error'
 urlpatterns = [
     path('', include('blog.urls')),
     path('auth/', include('django.contrib.auth.urls')),
-    path(
-        'auth/registration/',
-        CreateView.as_view(
-            template_name='registration/registration_form.html',
-            form_class=UserCreationForm,
-            success_url=reverse_lazy('blog:index'),
-        ),
-        name='registration',
-    ),
     path('pages/', include('pages.urls')),
     path('admin/', admin.site.urls),
 ]
